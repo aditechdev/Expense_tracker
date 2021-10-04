d6350ebf873cde09fb57bda6d8db67d5ff195d4e
aditechdev <aditechdevofficial@gmail.com>
2021-10-05 01:45:53 +0530

    :construction: Chart Under Progress

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({ Key key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          
+        ),
+      ],
+    );
+  }
+}
